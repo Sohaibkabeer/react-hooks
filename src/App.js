4d63c2135802aa8db51ac2ac7be0c6130ece4c58
Sohaibkabeer <sohaibkabeer@gmail.com>
2025-01-19 22:59:06 +0500

    useRef example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import {useState,useEffect} from 'react';
+import {useState,useEffect, useRef} from 'react';
 import './App.css';
 import Navbar from './component/Navbar';
 
@@ -9,12 +9,14 @@ function App() {
 
   // useState example
   const [color, setColor] = useState("Red")
+  const [count, setCount] = useState(0)
   const [message, setMessage] = useState("I am using a useState and its value is")
   const [name, setName] = useState("Learn React");
   const updateStates = () => {
     message === "I am using a useState and its value is" ? setMessage("Button pressed and the state has been ") : setMessage("I am using a useState and its value is");
     name === "Learn React" ? setName("Updated!!") : setName("Learn React");
     setColor("Yellow")
+    countRef.current = countRef.current +1;
   };
 //
 
@@ -26,6 +28,7 @@ useEffect(() => {
 
 useEffect(() => {
   alert("welcome!"); // basic effect will work on mount , runs 1 time at the time of app load
+  btnRef.current.style.backgroundColor = "red"
 }, [])
 
 
@@ -33,13 +36,17 @@ useEffect(() => {
   alert("name state changed to "+ name); // every time "name" state is changed it will be triggered. even at the start bcuz of init
 }, [name])
 
+// useRef example
+const countRef = useRef(0)
+const btnRef = useRef(0)  // we use useRef because across re-renders the value can persist, where as a state or local variable will change.
 
   return (
     <>
-    <Navbar color={color}/>
+    <Navbar color={color} countRef={countRef.current}/>
     <div className="App">
       <h1>{message} {name}</h1>
     <button onClick={()=>{updateStates()}}>Click me!</button>
+    <button ref={btnRef} onClick={()=>{setCount(count+1)}}>count is {count}</button>
     </div>
     </>
   );
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,16 +1,18 @@
-import { useEffect } from "react"
-import React from 'react'
+import { useEffect } from "react";
+import React from "react";
 
-const Navbar = ({color}) => {
+const Navbar = ({ color,countRef }) => {
+  // useEffect example
+  useEffect(() => {
+    alert("Color was changed"); // every time color is changed this will run
+  }, [color]);
 
-    // useEffect example
-    useEffect(() => {
-        alert("Color was changed") // every time color is changed this will run
-    }, [color])
-    
   return (
-    <div>The color of the navbar is {color}</div>
-  )
-}
+    <div>
+      <h1>The color of the navbar is {color}</h1>
+      <h2>Count : {countRef}</h2>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
